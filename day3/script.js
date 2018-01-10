ae10b37c0615f51de44a6a8bf95231ff67dfbbd6
He Wang <he@townsquare.agency>
2018-01-10 16:24:06 +1100

    delete day 3 js since it is not completed

--- day3/script.js
+++ day3/script.js
@@ -1,29 +1,4 @@
 var input = 289326
 
+// day 3 is not completed
 /* --- part 1 --- */
-
-var inputSqrt =  Math.sqrt(input)
-var inputSqrtWithInt = Math.floor( Math.sqrt(input) )
-var inputDirection = inputSqrt - inputSqrtWithInt
-var steps = 0
-
-
-if ( Number.isInteger( inputSqrtWithInt ) ) {
-	steps = inputSqrtWithInt - 1;
-}
-
-// calculate the how many levels between the input and the 1
-var levels = (inputSqrtWithInt - 1)
-
-console.log(levels)
-console.log(inputSqrtWithInt)
-console.log(Math.pow(inputSqrtWithInt, 2))
-console.log(input - Math.pow(inputSqrtWithInt, 2))
-console.log(inputSqrt)
-// console.log(steps)
-
-
-
-function round(value, decimals) {
-	return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
-}
