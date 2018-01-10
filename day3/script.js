d198aa7a2e1ea262ff953664ed25826364672c1a
He Wang <he@townsquare.agency>
2018-01-10 17:34:16 +1100

    start working on day 3 solution

--- day3/script.js
+++ day3/script.js
@@ -1,4 +1,16 @@
-var input = 289326
+// var input = 289326
 
 // day 3 is not completed
 /* --- part 1 --- */
+// formular is (2n + 1)^2
+
+function findCoordination(input) {
+	var target = 0
+	var x = 0,
+		y = 0
+
+	while ( target != input) {
+
+		target++
+	}
+}
